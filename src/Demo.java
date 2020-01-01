a1d7de9862d6fc48a662ccb9f0606c0c052c573c
zjw_global <zjw_global@qq.com>
2020-01-01 22:59:05 +0800

    第一次提交

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello IDEA!");
+        System.out.println("Hello IDEA!");
+        Date date = new Date();
+        System.out.println("");
+    }
+}
---
+++ src/Users.java
@@ -0,0 +1,20 @@
+public class Users {
+    private int userId;
+    private String passsword;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getPasssword() {
+        return passsword;
+    }
+
+    public void setPasssword(String passsword) {
+        this.passsword = passsword;
+    }
+}
